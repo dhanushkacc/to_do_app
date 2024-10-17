acaa8c386411c8715ce188a508bb392126c0d57a
dhanushkacc <136559309+dhanushkacc@users.noreply.github.com>
2024-10-17 17:18:16 +0530

    setup backend2

---
+++ server/models/UserModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt"); 
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+module.exports = UserModel;
--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
 mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.DB_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -23,7 +26,6 @@ mongoose
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
